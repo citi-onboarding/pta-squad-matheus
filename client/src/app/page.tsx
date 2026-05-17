ce56eede9c06edb58ec5e36d2e0c5cc4d6204516
Matheus Henrique <matheushcavalcanti@hotmail.com>
2026-05-17 18:26:28 -0300

    fix: bookCard component folder correction
    bookCard component folder correction and fix some little other things.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,40 +1,11 @@
 import { Header } from '@/components/layout/Header';
-import { LastLoansTable } from '@/components/last-loans-table/index';
-import { BookCard } from '@/components/layout/book/BookCard';
 
 export default function TestPage() {
   return (
     <div>
       <Header />
-      <div className="p-10 grid grid-cols-3 gap-4 w-[70%] mx-auto">
-        <BookCard
-          title="Livro 1"
-          author="Autor 1"
-          category="Genero 1"
-          availableQuantity={1}
-          coverUrl="/img/Ciencias.png"
-        />
-        <BookCard
-          title="Livro 2"
-          author="Autor 2"
-          category="Genero 2"
-          availableQuantity={2}
-          coverUrl="/img/Ciencias.png"
-        />
-        <BookCard
-          title="Livro 3"
-          author="Autor 3"
-          category="Genero 3"
-          availableQuantity={3}
-          coverUrl="/img/Ciencias.png"
-        />
-        <BookCard
-          title="Livro 4"
-          author="Autor 4"
-          category="Genero 4"
-          availableQuantity={4}
-          coverUrl="/img/Ciencias.png"
-        />
+      <div className="p-10">
+        <p>Conteúdo da página aqui...</p>
       </div>
     </div>
   );
