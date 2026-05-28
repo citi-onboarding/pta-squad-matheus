5a130b21285ccef3a238a3d05fc745e1b18d6825
guilhermetn <gui222013@hotmail.com>
2026-05-28 10:54:29 -0300

    feat: add reverse proxy to forward /api requests to backend

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js']
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*',
+      },
+    ];
+  },
 };
