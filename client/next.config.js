e82345a890a151938f867eb73f9f1066eb433066
Matheus Henrique <matheushcavalcanti@hotmail.com>
2026-06-06 04:08:11 -0300

    fix: dependencies

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:3001';
+
 module.exports = {
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*',
+        destination: `${BACKEND_URL}/api/:path*`,
       },
     ];
   },
